70fbb2c4fd2afd09646a4261b00e233bdcfa3138
German Arcila <germanarcila@gmail.com>
2016-04-06 08:54:48 -0400

    view all create and add in process

--- app/actions.rb
+++ app/actions.rb
@@ -8,5 +8,16 @@ end
 
 get "/contacts" do
 	content_type :json
-	CONTACTS.to_json
+	Contact.all.to_json
 end
+
+post "/contacts" do
+	@contact = Contact.new(params)
+	@contact.save
+end
+
+post "/contacts/search" do
+	content_type :json
+	Contact.first.to_json	 #needs to be find, not .first ojo, change it sir.
+end
+
